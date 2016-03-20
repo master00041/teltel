8a01d568bb240f3ce4310eff079fe0d9ac04e78d
master00041 <master041amin@gmail.com>
2016-03-20 23:48:55 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -245,7 +245,7 @@ function create_config( )
       "portuguese_lang",
       "arabic"
      },
-    sudo_users = {our_id},
+    sudo_users = {104583328},
     admin_users = {},
     disabled_channels = {}
   }
